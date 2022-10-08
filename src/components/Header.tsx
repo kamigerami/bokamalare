16bd1043bfadbf90f230f5bcc495d6911bac4e69
kami gerami <kami.gerami@gmail.com>
2022-10-08 22:30:59 +0200

    remove props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,7 @@ const navigation = [
   { name: "Vanliga frågor", href: "#" },
 ];
 
-type Props = {}
-
-function Header({}: Props) {
+function Header() {
   return (
     <div className="relative overflow-hidden bg-white">
     <div className="mx-auto max-w-7xl">          
